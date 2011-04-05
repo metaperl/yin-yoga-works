38493a5e64fc527bf71141bb8025d435c8b0dec2
Terrence Brannon <metaperl@gmail.com>
2011-04-05 08:00:10 -0400

    xml generation

--- grab-asanas.py
+++ grab-asanas.py
@@ -13,6 +13,7 @@ soup = BeautifulSoup(html)
 #print soup.prettify()
 
 lis = soup.findAll('li')
+lis = lis[:3]
 
 import re
 
@@ -22,7 +23,7 @@ for li in lis:
     asana = {}
     a = li.find('a')
     print a['href'], "\t", a.string
-    asana['title'] = a.string
+    asana['name'] = a.string
     asana['page_url'] = base_url + a['href']
     asana_page_html = urllib.urlopen(asana['page_url']).read()
     soup = BeautifulSoup(asana_page_html)
@@ -34,3 +35,33 @@ for li in lis:
 import json
 
 print json.dumps(asanas, indent=4)
+
+from lxml import etree
+root = etree.Element("asanas")
+
+def textelem(tag_name, text):
+    e = etree.Element(tag_name)
+    e.text = text
+    return e
+
+for asana in asanas:
+    ae = etree.Element("asana")
+    for key in asana:
+        tmp = textelem(key, asana[key])
+        ae.append(tmp)
+    root.append(ae)
+
+print(etree.tostring(root, pretty_print=True))
+
+
+
+# import sys
+# sys.path.append('/home/thequietcenter/prg/web2py')
+
+# from gluon.dal import DAL, Field
+
+# db = DAL('sqlite://storage.db')
+
+# db.asanas.bulk_insert(asanas)
+
+
